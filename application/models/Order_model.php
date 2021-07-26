54137e8d1a96e3881f4bbf5c86528fd408dded4a
lamiral <lev-197508@mail.ru>
2021-07-26 21:34:37 +0700

    Add files via upload

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -1,46 +1,114 @@
 <?php
 
 class Order_model extends CI_Model
-{
+{	
+	const ORDERS_TABLE 	     = 'Orders';
+	const ORDERS_ITEMS_TABLE = 'orders_items';
+	const ORDER_ID           = 'order_id';
+	
+	/*
+		Метод Order_model::is_select_ok() 
+			проверяет наличие записей в результате запроса
+	*/
+	
+	private function is_select_ok($query)
+	{
+		if($query->num_rows() == 0) return false;
+		else 					    return true;
+	}
+	
 	public function __construct()
 	{
 		$this->load->database();
 	}
-
+	
+	/*
+		Метод Order_model::get_order_by_id() 
+			возвращает шапку заказа по данному id заказа.
+	*/
+	
 	public function get_order_by_id($order_id)
 	{	
-		$sql = 'SELECT * FROM my_order WHERE order_id = ?';
+		$sql = 'SELECT * FROM orders WHERE order_id = ?';
 		$query = $this->db->query($sql,array($order_id));
-
-		return $query->result();
+		
+		if(!$this->is_select_ok($query)) return false;
+		return $query->row();
 	}
-
+	
+	/*
+		Метод Order_model::get_orderItems_by_id() 
+			возвращает строки заказа по данному id заказа
+	*/
+	
 	public function get_orderItems_by_id($order_id)
 	{
-		$sql = 'SELECT * FROM order_item WHERE order_id = ?';
-
-		$query = $this->db->query($sql,array($order_id));
-
+		$sql1 = 'SELECT orders_items.id,orders_items.product_id,orders_items.order_id,orders_items.product_count,products.price
+					FROM orders_items,products 
+						WHERE products.product_id = orders_items.product_id AND orders_items.order_id = ?';
+		$sql = "SELECT * FROM orders_items WHERE order_id = ?";
+		
+		$query  = $this->db->query($sql1,array($order_id));
+		
+		if(!$this->is_select_ok($query)) return false;
+		
 		return $query->result();
 	}
 
-	public function create($client,$header,$products)
+	/*
+		Метод Order_model::create() записывает заказ в БД
+	*/
+	
+	public function create($header,$order_items)
 	{	
-		#$sql = 'INSERT INTO order(status,client_name,client_number,comment) VALUES ('.$this->db->escape($header['status']).','.$this->db->escape($header['comment'].')';
-		$this->db->query($sql,array());
-
-		foreach($products as $product)
-		{	
-			$sql = "INSERT INTO Products "
+		$this->db->trans_start();
+		
+		$order_date = array();
+	
+		foreach($header as $header_field => $value)
+		{
+			$order_date[$header_field] = $value;
 		}
+		
+		$query = $this->db->insert(self::ORDERS_TABLE,$order_date);
 
-		#$this->db->
+		if(!$query)
+		{
+			return false;
+		}
+		
+		$order_insert_items = array();
+		
+		foreach($order_items as $order_item)
+		{	
+			$order_item = array(
+				'product_id' 	=> $order_items['product_id'],
+				'order_id'   	=> $header["order_id"],
+				'product_count' => $order_items['product_count']
+			);
+			array_push($order_insert_items,$order_item);
+		}
+		
+		$result = $this->db->insert(self::ORDERS_ITEMS_TABLE,$order_insert_items);
+		
+		$this->db->trans_complete();
 	}
 
+	/*
+		Метод Order_model::cansel() отменяет заказ с данным id
+	*/
+	
 	public function cansel($order_id)
 	{	
-		$query = $this->db->query('DELETE FROM `order` WHERE order_number = '.$order_id);
-		$query = $this->db->query('DELETE FROM `order_item` WHERE order_id = '.$order_id);
+		$update = array(
+			'status' => 'Отменен'
+		);
+		
+		$this->db->where(self::ORDER_ID,$order_id);
+		$query = $this->db->update(SELF::ORDERS_TABLE,$update);
+		
+		if($query) return true;
+		else       return false;
 	}
 }
 
--- application/models/Products_model.php
+++ application/models/Products_model.php
@@ -1,119 +1,259 @@
 <?php
 
 class Products_model extends CI_Model
-{	
+{		
+	/* Табличные константы */
+	
+	const IMAGE_TABLE 	 = 'products_images';
+	
+	const PRODUCTS_TABLE = 'products';
+	const PRODUCT_ID     = 'product_id';
+	const PRODUCT_GROUP  = 'group';
+	
 	public function __construct()
 	{
 		$this->load->database();
 	}
-
+	
 	/*
-		Вспомогательный метод. Возвращает массив картинок продукта с идентификатором product_id
+		Метод Products_model::is_select_ok()
+			проверяет наличие записей в результате запроса
 	*/
-
-	private function get_images($product_id)
+	
+	private function is_select_ok($query)
 	{
-		$sql = "SELECT hight_url, low_url FROM images WHERE product_id = ?";
-
-		$query = $this->db->query($sql,array($product_id));
-
-		return $query->result();
+		if($query->num_rows() == 0) return false;
+		else 					    return true;
 	}
 
 	/*
-		Вспомогательный метод. Формирует результирующий вид продукта - данные + картинки.
+		Метод Products_model::get_all() 
+			возвращает массив всех картинок для всех продуктов 
 	*/
+	
+	private function get_all_images()
+	{
+		$images = $this->db->get(self::IMAGE_TABLE);
+		return $images;
+	}
 
-	private function create_result_products($products)
+	/*
+		Метод Products_model::get_all_images_group()
+			возвращает массив всех картинок для всех продуктов из данной группы
+	*/
+	
+	private function get_all_images_group($group)
+	{
+		$sql = 'SELECT products_images.low_url,products_images.hight_url,products_images.product_id 
+					FROM products_images 
+						WHERE products_images.product_id IN(
+							SELECT products.product_id FROM products WHERE products.group = '.$this->db->escape($group).')';
+		
+		$images = $this->db->query($sql);
+		return $images->result();
+	}
+	
+	/*
+		Метод Products_model::get_images_by_id() 
+				возвращает массив картинок продукта с идентификатором product_id
+	*/
+	
+	private function get_images_by_id($product_id)
+	{
+		$images = $this->db->from(self::IMAGE_TABLE);
+		$images = $this->db->where(self::PRODUCT_ID,$product_id);
+		$images = $images->get();
+		
+		return $images->result();
+	}
+	
+	/*
+		Метод Products_model::get_all_products() 
+			возвращает все продукты(товары)
+	*/
+	private function get_all_products()
+	{
+		$products = $this->db->get(self::PRODUCTS_TABLE);
+		
+		return $products;
+	}
+	
+	/*
+		Метод Products_model::get_products_group(() 
+			возвращает все продукты(товары) из данной группы
+	*/
+	
+	private function get_products_group($group)
+	{
+		$products = $this->db->from(self::PRODUCTS_TABLE);
+		$products = $this->db->where(self::PRODUCT_GROUP,$group);
+		
+		$products = $this->db->get();
+
+		return $products;
+	}
+	
+	/*
+		Метод Products_model::get_product_by_id() 
+			возвращает продукт(товар) по идентификатору(id)
+	*/
+	
+	private function get_product_by_id($product_id)
+	{
+		$product = $this->db->where(self::PRODUCT_ID,$product_id);
+		$product = $this->db->from(self::PRODUCTS_TABLE);
+
+		return $this->db->get();
+	}
+	
+	/*
+		Метод Products_model::esult_product() 
+			формирует "вид" товара
+	*/
+	
+	private function result_product($product)
+	{		
+		$result_product = array();
+			
+		foreach($product as $table_field => $value_field)
+		{
+			$result_product[$table_field] = $value_field;
+		}
+			
+		$result_product['images'] = array();
+			
+		return $result_product;
+	}
+	
+	/*
+		Метод Products_model::create_result_product()
+			возвращает результирующий вид продукта, вместе с картинками
+	*/
+	
+	private function create_result_product($product,$images)
+	{
+		$result_product = $this->result_product($product);
+		$id = self::PRODUCT_ID;
+		foreach($images as $image)
+		{	
+			if($image->$id == $product->$id)
+			{	
+				array_push($result_product['images'],$image);
+			}
+		}	
+		return $result_product;
+	}
+		
+	/*
+		Метод Products_model::create_result_products()
+			возвращает результирующий вид продуктов, вместе с картинками
+	*/
+	
+	private function create_result_products($products,$images)
 	{
 		$result_products = array();
 
-
 		foreach($products as $product)
 		{
-			$result_product = array(
-				'id'  	 => $product->id,
-				'name'   => $product->name,
-				'price'  => $product->price,
-				'group'  => $product->group_product,
-				'info'   => $product->info,
-				'images' => array()				// Массив картинок
-				);
+			$result_product = $result_product = $this->result_product($product);
 
-			foreach ($this->get_images($product->id) as $image)
+			foreach ($images as $image)
 			 {	
-			 	$img = array(
-			 		'low_url' 	=> $image->low_url,
-			 		'hight_url' =>$image->hight_url
-			 		);
-				array_push($result_product['images'], $img);	
+				array_push($result_product['images'], $image);	
 			 }
-
 			 array_push($result_products,$result_product);
 		}
-
 		return $result_products;
 	}
-
+	
+	/******************* ИНТЕРФЕЙС КЛАССА Products_model **************************/
+	
+	/*
+		Метод Products_model::get_price_by_id() 
+			возвращает цену товара(продукта) с данным id товара
+	*/
 	public function get_price_by_id($product_id)
 	{
-		$sql = "SELECT price FROM Products WHERE id = ?";
+		$sql = "SELECT price FROM products WHERE product_id = ?";
 		$price_query = $this->db->query($sql,array($product_id));
-
-		$price = $price_query->result()[0]->price;
+		
+		$price = $price_query->row();
 
 		return $price;
 	}
-
-
-	public function get_all()
+	
+	/*
+		Метод Products_model::get_price_by_order() 
+			возвращает цену товара(продукта) с данным id заказа
+	*/
+	
+	public function get_price_by_order($order_id)
 	{
-		$query = $this->db->get('Products');
-		$products = $query->result();
+		$sql = "SELECT products.price,products.product_id FROM products 
+					WHERE products.product_id IN
+						(SELECT orders_items.product_id  FROM orders_items 
+							WHERE orders_items.order_id = ".$this->db->escape($order_id).")";
+		$prices = $this->db->query($sql);
+		return $prices->result();
+	}
 
-		$result_products = $this->create_result_products($products);
-
+	/*
+		Метод Products_model::get_all() 
+			возвращает все товары
+	*/
+	
+	public function get_all()
+	{	
+		$products = $this->get_all_products();
+		$images   = $this->get_all_images();
+		
+		$result_products = $this->create_result_products($products->result(),$images->result());
+		
 		return $result_products;
 	}
-
+	
+	/*
+		Метод Products_model::get_by_id() 
+			возвращает товар с данным id
+				В случае, если товара с данным id не в базе, 
+					вернется ответ "Нет товара с таким id"
+	*/
+	
 	public function get_by_id($product_id)
-	{	
-		$product_id = $this->db->escape_str($product_id);
-
-		$query = $this->db->query('SELECT * FROM Products WHERE id ='.$product_id);
-		$product = $query->result();
-		$product = $product[0];
-
-		$result_product = array(
-				'id'  	 => $product->id,
-				'name'   => $product->name,
-				'price'  => $product->price,
-				'group'  => $product->group_product,
-				'info'   => $product->info,
-				#'images' => array()
-				);
-			/*foreach ($this->get_images($product->id) as $image)
-			 {	
-			 	$img = array(
-			 		'low_url' 	=> $image->low_url,
-			 		'hight_url' =>$image->hight_url
-			 		);
-				array_push($result_product['images'], $img);	
-			 }
-			*/
-
+	{
+		$product = $this->get_product_by_id($product_id);
+		$images  = $this->get_images_by_id($product_id);
+		
+		if(!$this->is_select_ok($product))
+		{
+			$answer = "Нет товара с таким id";
+			return $answer;
+		}
+		
+		$result_product = $this->create_result_product($product->row(),$images);
+		
 		return $result_product;
 	}
-
-	public function get_group($group)
+	
+	/*
+		Метод Products_model::get_by_group() возвращает товары из данной группы
+			В случае, если товаров в данной группе нет в базе, 
+					вернется ответ "Нет заказов с такой группы".
+	*/
+	
+	public function get_by_group($group)
 	{	
-		$group = $this->db->escape_str($group);
-
-		$sql = 'SELECT * FROM Products WHERE group_product= ? ';
-		$products_group = $this->db->query($sql,array($group));
-
-		$result_products_group = $this->create_result_products($products_group->result());
-
+		$images   = $this->get_all_images_group($group);
+		$products = $this->get_products_group($group);
+		
+		if(!$this->is_select_ok($products))
+		{
+			$answer = "Нет заказов с такой группы";
+			return $answer;
+		}
+		
+		$result_products_group = $this->create_result_products($products->result(),$images);
+		
 		return $result_products_group;
 	}
 }
