6a3fa26224486b0db4ec6b217cabdf56f1104b7f
lamiral <lev-197508@mail.ru>
2021-07-26 21:37:36 +0700

    Update TaskBase.sql

--- TaskBase.sql
+++ TaskBase.sql
@@ -2,31 +2,31 @@ CREATE DATABASE IF NOT EXISTS TaskTable;
 
 CREATE TABLE IF NOT EXISTS `products`
 (
-	`product_id` int(10) 	 NOT NULL AUTO_INCREMENT PRIMARY KEY , 
+	`product_id` 	 int(10)     NOT NULL AUTO_INCREMENT PRIMARY KEY , 
 	`name`		 varchar(25) NOT NULL,
 	`group`		 varchar(25) NOT NULL,
-	`price`		 int(10)	 NOT NULL,
-	`info`		 text 		 NOT NULL
+	`price`		 int(10)     NOT NULL,
+	`info`		 text 	     NOT NULL
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `products_images`
 (	
-	`image_id`	 int(10)	NOT NULL  AUTO_INCREMENT,
-	`product_id` int(10)	NOT NULL ,
-	FOREIGN KEY (`product_id`)  REFERENCES products(`product_id`) ON DELETE CASCADE,
-	`hight_url`  varchar(40) NOT NULL,
+	`image_id`	 int(10)     NOT NULL  AUTO_INCREMENT,
+	`product_id`     int(10)     NOT NULL ,
+	FOREIGN KEY (`product_id`)   REFERENCES products(`product_id`) ON DELETE CASCADE,
+	`hight_url`      varchar(40) NOT NULL,
 	`low_url`	 varchar(40) NOT NULL
 
 )ENGINE=InnoDB;
 	
 CREATE TABLE IF NOT EXISTS `orders`
 (
-  `order_id` 		int(10) 	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `order_id` 		int(10)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `status`   		varchar(25) NOT NULL,
   `client_name` 	varchar(25) NOT NULL,
   `client_number` 	varchar(12) NOT NULL,
   `comment` 		text 	    NOT NULL,
-  `date` 			DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date` 		DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `orders_items`
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `orders_items`
 
 CREATE TABLE IF NOT EXISTS `clients`
 (
-  `id` 		 int(11) 	 NOT NULL,
-  `number` 	 int(11) 	 NOT NULL,
-  `password` varchar(25) NOT NULL
+  `id` 		 int(11)     NOT NULL,
+  `number` 	 int(11)     NOT NULL,
+  `password` 	 varchar(25) NOT NULL
 )ENGINE=InnoDB;
